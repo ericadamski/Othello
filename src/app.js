5b07bcf8e73962fb4c02ba2b991cd15ac64326f9
Eric Adamski <er.adamski@gmail.com>
2015-03-01 13:31:02 -0500

    Made interface more useable, Add ability to restart a new game

--- src/app.js
+++ src/app.js
@@ -6,40 +6,36 @@
 //
 // Initialization after the page is loaded
 //
-$(document).ready(function () {
+Game = {
 
+  boardDimension: 8,
+	elements: [],
+	board: null,
 
-  var boardDimension = 8;
+  getGameMode : function() {
 
-  // create elements
-	var elements = [];
+  },
 
-	// create a new board
-	var board = new Board(boardDimension);
+  updateGuessLocations : function() {
+  	Game.elements.forEach(function(cells, row) {
+  		cells.forEach(function(cell, col) {
+ 				if (Game.board.verifyMove(new Coordinate(row, col)))
+ 					cell.renderGuess(true);
+ 				else
+		 		  cell.renderGuess(false);
+  		});
+  	});
+  },
 
-	for (var row = 0; row < boardDimension; row++) {
-		elements.push(Array.dim(boardDimension).map(function () {
-			var box = $("<div>");
-			box.addClass("box");
-			$("#board").append(box);
-			return box;
-		}));
-	};
-
-  var getGameMode = function() {
-    return $('input[name=mode]:checked', '#gameMode').val();
-  }
-
-	var updateTurnLabel = function(player) {
+	updateTurnLabel : function(player) {
 		$("#turn").html("");
     var imageSrc = 'images/' + player.toString() + '.png';
     var image = $('<img>').attr('src', imageSrc);
-
 		$("#turn").prepend(image).prepend(player.toString());
-	};
+	},
 
-	var updateScoreLabel = function() {
-		var player = board.getCurrentPlayer();
+	updateScoreLabel : function() {
+		var player = Game.board.getCurrentPlayer();
 		var scoreLabelId = player.scoreLabelId;
 		var score = player.score;
 		var playerString = player.toString();
@@ -48,115 +44,129 @@ $(document).ready(function () {
 		score = player.other.score;
 		playerString = player.other.toString();
 		$("#" + scoreLabelId).html(score);
-	};
+	},
 
-  var renderPiece = function(player, row, col) {
-    elements[row][col].html("");
+  renderPiece : function(player, row, col) {
+    this.elements[row][col].html("");
     var imageSrc = 'images/' + player.toString() + '.png';
     var image = $('<img>').attr('src', imageSrc);
-    elements[row][col].prepend(image);
-  };
+    this.elements[row][col].prepend(image);
+  },
 
-  renderPiece(board.getPlayer(0), 3, 3);
-  renderPiece(board.getPlayer(1), 3, 4);
-  renderPiece(board.getPlayer(1), 4, 3);
-  renderPiece(board.getPlayer(0), 4, 4);
+  start : function() {
+    Game.boardDimension = 8;
+    Game.elements = [];
+    Game.board = new Board(Game.boardDimension);
 
-	updateTurnLabel(board.getCurrentPlayer());
-	updateScoreLabel();
+    for (var row = 0; row < Game.boardDimension; row++) {
+    	Game.elements.push(Array.dim(Game.boardDimension).map(function () {
+    		var box = $("<div>");
+    		box.addClass("box");
+    		$("#board").append(box);
+    		return box;
+    	}));
+    }
 
-	// attach renderGuess methods to elements
-	elements.forEach(function(cells, row) {
-		cells.forEach(function(cell, col) {
-			cell.renderGuess = function (switchOn) {
-				if (switchOn) {
-					cell.addClass("guess");
-				} else {
-					cell.removeClass("guess");
-				}
-			};
-		});
-	});
+    Game.renderPiece(Game.board.getPlayer(0), 3, 3);
+    Game.renderPiece(Game.board.getPlayer(1), 3, 4);
+    Game.renderPiece(Game.board.getPlayer(1), 4, 3);
+    Game.renderPiece(Game.board.getPlayer(0), 4, 4);
 
-	// attach render methods to elements
-	elements.forEach(function(cells, row) {
-		cells.forEach(function(cell, col) {
-			cell.render = function (player) {
-        cell.renderGuess(false);
-        renderPiece(player, row, col);
-			};
-		});
-	});
+  	Game.updateTurnLabel(Game.board.getCurrentPlayer());
+  	Game.updateScoreLabel();
 
-  // attach deRender methods to elements
-  elements.forEach(function(cells, row) {
-    cells.forEach(function(cell, col) {
-      cell.deRender = function () {
-        cell.html("");
-      };
-    });
-  });
+  	// attach renderGuess methods to elements
+  	Game.elements.forEach(function(cells, row) {
+  		cells.forEach(function(cell, col) {
+  			cell.renderGuess = function (switchOn) {
+  				if (switchOn) {
+  					cell.addClass("guess");
+  				} else {
+  					cell.removeClass("guess");
+  				}
+  			};
+  		});
+  	});
 
-	//attach hover method
-	elements.forEach(function(cells, row) {
-		cells.forEach(function(cell, col) {
-			 cell.hover(
-			 	function() {
-	 				if (board.verifyMove(new Coordinate(row, col))) {
-	 					cell.renderGuess(true);
-	 				}
-			 	},
-			 	function() {
-			 		cell.renderGuess(false);
-			 });
-		});
-	});
+  	// attach render methods to elements
+  	Game.elements.forEach(function(cells, row) {
+  		cells.forEach(function(cell, col) {
+  			cell.render = function (player) {
+          cell.renderGuess(false);
+          Game.renderPiece(player, row, col);
+  			};
+  		});
+  	});
 
-
-  var getElementAt = function (coordinate) {
-    return elements[coordinate.getRow()][coordinate.getColumn()];
-  };
-
-
-  elements.forEach(function(cells, row, elements) {
-    cells.forEach(function(cell, col) {
-      cell.click(function() {
-        var coordinate = new Coordinate(row,col);
-        if (getElementAt(coordinate).hasClass("guess")) {
-          var move = board.play(coordinate);
-          renderLine(move.getAllUpdatedCoordinates(), move.getPlayer());
-          updateTurnLabel(board.getCurrentPlayer());
-          updateScoreLabel();
-          if (board.isGameOver()) {
-            gameOver();
-            return;
-          }
-
-          if (getGameMode() =="hvc") {
-            move = board.play(); /////////// TODO: Put in AI stuff herehrehr::::////
-            renderLine(move.getAllUpdatedCoordinates(), move.getPlayer());
-            updateTurnLabel(board.getCurrentPlayer());
-            updateScoreLabel();
-            if (board.isGameOver()) gameOver();
-          }
-
+    // attach deRender methods to elements
+    Game.elements.forEach(function(cells, row) {
+      cells.forEach(function(cell, col) {
+        cell.deRender = function () {
+          cell.html("");
         };
       });
     });
-  });
+
+    Game.updateGuessLocations();
+
+    Game.elements.forEach(function(cells, row, elements) {
+      cells.forEach(function(cell, col) {
+        cell.click(function() {
+          var coordinate = new Coordinate(row,col);
+          if (Game.getElementAt(coordinate).hasClass("guess")) {
+            var move = Game.board.play(coordinate);
+            Game.renderLine(move.getAllUpdatedCoordinates(), move.getPlayer());
+            Game.updateTurnLabel(Game.board.getCurrentPlayer());
+            Game.updateScoreLabel();
+            Game.updateGuessLocations();
+            if (Game.board.isGameOver()) {
+              Game.gameOver();
+              return;
+            }
+          }
+            /////////// TODO: Put in AI stuff herehrehr::::////
+        });
+      });
+    });
+    Game.gameOver();
+  },
+
+  getElementAt : function (coordinate) {
+    return Game.elements[coordinate.getRow()][coordinate.getColumn()];
+  },
 
   //takes a list of coordinates and what player to render
-  var renderLine = function (coordinates, player) {
+  renderLine : function (coordinates, player) {
     coordinates.forEach( function (coordinate, index, coordinates) {
-      getElementAt(coordinate).render(player);
+      Game.getElementAt(coordinate).render(player);
     })
-  };
+  },
 
-  var gameOver = function () {
-      var winner = board.getWinner();
-      $("#outcome").html("Winner: " + winner.toString());
-      updateTurnLabel(winner);
-			//draw a restart button
+  gameOver : function () {
+      //var winner = Game.board.getWinner();
+      var outcome = $("#outcome");
+      //outcome.html("Winner: " + winner.toString());
+
+      var reset = document.createElement('button');
+      reset.id = 'restart-button';
+      reset.name = 'Restart';
+      reset.innerHTML = 'Restart';
+      reset.class = '.btn';
+      reset.onclick = Game.reset;
+
+      var out = document.getElementById('outcome');
+      out.appendChild(document.createElement('br'));
+      out.appendChild(reset);
+
+      //this.updateTurnLabel(winner);
+  },
+
+  reset : function() {
+    $("#board").empty();
+    $("#outcome").empty();
+    Game.start();
   }
 
-});
+};
+
+$(document).ready(Game.start);
